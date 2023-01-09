68dec5d4c631605d8db3f527544662af7e205c00
Rohan <rohanmolinillo@gmail.com>
2023-01-09 11:36:30 +0100

    Creating route for deleting user and movie

--- movieHandlers.js
+++ movieHandlers.js
@@ -100,6 +100,28 @@ const updateMovie = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+
+    .query("delete from movies where id = ?", [id])
+
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+
+    .catch((err) => {
+      console.error(err);
+
+      res.status(500).send("Error deleting the movie");
+    });
+};
+
 module.exports = {
   getMovies,
 
@@ -108,4 +130,6 @@ module.exports = {
   postMovie,
 
   updateMovie,
+
+  deleteMovie,
 };
